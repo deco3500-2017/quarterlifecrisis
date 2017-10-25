c4f73b879358e0954687dc146bc22fdc07ba89a3
Daniel Homer <daniel.homer@uqconnect.edu.au>
2017-10-25 12:05:55 +1000

    Added average function. getRatingAverage will return the average of the ratings given

--- Interactive Prototype/js/custom.js
+++ Interactive Prototype/js/custom.js
@@ -14,11 +14,12 @@ function showComments(){
 
 //function displayDate() {
 //    var likeLabel = parseInt( document.getElementById("demo").innerHTML);
-//    
+//
 //    likeLabel = likeLabel + 1;
-//    
+//
 //    document.getElementById("demo").innerHTML = likeLabel;
 //}
+
 
 var class_names= document.getElementsByClassName("fa-star");
 
@@ -32,36 +33,70 @@ var currentRating = 0;
 function starRating() {
     var element = this;
     var newRating = parseInt(this.id);
-    
+
     if(previousRating == 1 && element.classList.contains('iconActive')){
+      // limit to rating of 0
             for (var i = 1; i < 6; i++) {
-            document.getElementById(i).classList.remove("iconActive"); 
+            document.getElementById(i).classList.remove("iconActive");
         }
         previousRating = 0;
         currentRating = 0;
         console.log(currentRating);
         document.getElementById("profileRating").innerHTML = currentRating;
+        addToAvgList(currentRating);
+
     } else if(element.classList.contains('iconActive')){
+      // lower rating
         for (var i = 5; i > newRating; i--) {
-            document.getElementById(i).classList.remove("iconActive"); 
+            document.getElementById(i).classList.remove("iconActive");
         }
         previousRating = newRating;
         currentRating = newRating;
         console.log(currentRating);
         document.getElementById("profileRating").innerHTML = currentRating;
+        addToAvgList(currentRating);
     } else {
+      // higher rating
         for (var i = 1; i < newRating + 1; i++) {
-            document.getElementById(i).classList.add("iconActive"); 
+            document.getElementById(i).classList.add("iconActive");
         }
         previousRating = newRating;
         currentRating = newRating;
         console.log(currentRating);
         document.getElementById("profileRating").innerHTML = currentRating;
+        addToAvgList(currentRating);
     }
+
+
+}
+
+var avg_list = [];
+
+function addToAvgList(num){
+  avg_list.push(num);
+  console.log(avg_list);
+  var x = getRatingAverage();
+  console.log(x);
+}
+
+
+
+
+
+
+function getRatingAverage(){
+  var total = 0; // reset total
+  for (var i = 0; i < avg_list.length; i++){
+    total += avg_list[i]; // iterate through avg_list
+  }
+  //console.log(total);
+  var avg = total / avg_list.length;
+  // return average
+  return avg;
 }
 
 function search(ele) {
     if(event.key === 'Enter') {
-        document.getElementById("commentText").innerHTML = ele.value + "<br><i id='1' class='fa fa-star' aria-hidden='true'></i><i id='2' class='fa fa-star' aria-hidden='true'></i><i id='3' class='fa fa-star' aria-hidden='true'></i><i id='1' class='fa fa-star' aria-hidden='true'></i><i id='4' class='fa fa-star' aria-hidden='true'></i><i id='5' class='fa fa-star' aria-hidden='true'></i>" 
+        document.getElementById("commentText").innerHTML = ele.value + "<br><i id='1' class='fa fa-star' aria-hidden='true'></i><i id='2' class='fa fa-star' aria-hidden='true'></i><i id='3' class='fa fa-star' aria-hidden='true'></i><i id='1' class='fa fa-star' aria-hidden='true'></i><i id='4' class='fa fa-star' aria-hidden='true'></i><i id='5' class='fa fa-star' aria-hidden='true'></i>"
     }
 }
