7ed693299916a1568402897cb0bdb12e8ca4a04c
Daniel Homer <daniel.homer@uqconnect.edu.au>
2017-10-25 16:30:20 +1000

    fin

--- Interactive Prototype/js/custom.js
+++ Interactive Prototype/js/custom.js
@@ -71,15 +71,38 @@ function starRating() {
 }
 
 // list of ratings
-var avg_list = [];
+//var avg_list = [];
+var seb_avg_list = [];
+var dan_avg_list = [];
+var liam_avg_list = [];
+var mitch_avg_list = [];
+var saj_avg_list = [];
 
 function addToAvgList(num){
-  avg_list.push(num); // add to list of ratings
+    var user = getUser();
+    if (user == "dan"){
+        dan_avg_list.push(num);
+        document.getElementById("danrating").innerHTML = getRatingAverage(dan_avg_list);
+    } else if (user == "seb"){
+        seb_avg_list.push(num);
+        document.getElementById("sebrating").innerHTML = getRatingAverage(seb_avg_list);
+    } else if (user == "liam"){
+        liam_avg_list.push(num);
+        document.getElementById("liamrating").innerHTML = getRatingAverage(liam_avg_list);
+    }  else if (user == "saj"){
+        saj_avg_list.push(num);
+        document.getElementById("sanrating").innerHTML = getRatingAverage(saj_avg_list);
+    } else if (user == "mitch"){
+        mitch_avg_list.push(num);
+        document.getElementById("mitchrating").innerHTML = getRatingAverage(mitch_avg_list);
+    }
+
+    //seb_avg_list.push(num);
+   // avg_list.push(num); // add to list of ratings
  //
 // get current rating for appending
 
 
-  var x = getRatingAverage();
 }
 
 
@@ -87,7 +110,10 @@ function addToAvgList(num){
 
 
 
-function getRatingAverage(){
+
+
+
+function getRatingAverage(avg_list){
   var total = 0; // reset total
   for (var i = 0; i < avg_list.length; i++){
     total += avg_list[i]; // iterate through avg_list
@@ -109,6 +135,8 @@ function getRatingAverage(){
 
 $("input").on("keydown",function search(e) {
     if(e.keyCode == 13) {
+        var id = $(this).attr('id');
+        setUser(id);
         // enter key pressed
         var comment = $(this).val();
         if (comment != "") {
@@ -164,13 +192,53 @@ $("input").on("keydown",function search(e) {
 
             document.getElementById("commentText").appendChild(newcontent);           // Append <p> to <div> with id="myDIV"
 
-            cleanUp();
         }
+        updateAvg(id);
+        //updateValue
+        cleanUp();
+
     }//else {
        // alert("Please enter a comment!");
         //doc
     //}
 });
+
+var usr;
+
+function setUser(id){
+    switch(id) {
+        case "usrseb":
+            usr = "seb";
+            break;
+        case "usrdan":
+            usr = "dan";
+
+            break;
+        case "usrliam":
+            usr = "liam";
+
+            break;
+        case "usrmitch":
+            usr = "mitch";
+
+            break;
+        case "usrsaj":
+            usr = "saj";
+
+            break;
+        default:
+
+    }
+}
+
+function getUser(){
+    return usr;
+}
+
+
+function updateAverage(id){
+
+}
 
 
 function cleanUp(){
@@ -190,7 +258,14 @@ function cleanUp(){
 }
 
 
-
+$('.tag').click(function(){
+    $(".newsfeed .card").hide();
+    var search_term = $(this).text();
+    console.log(search_term);
+    console.log($("div div button:contains("+search_term+")"));
+    var object = $("button:contains("+search_term+")").parent();
+    $(".newsfeed").append(object);
+});
 
 
 
@@ -198,11 +273,13 @@ function cleanUp(){
 
 
 function search(ele) {
-    if(event.key == 'Enter') {
+    /*if(event.key == 'Enter') {
         document.getElementById("commentText").innerHTML = ele.value + "<br><i id='1' class='fa fa-star' aria-hidden='true'></i><i id='2' class='fa fa-star' aria-hidden='true'></i><i id='3' class='fa fa-star' aria-hidden='true'></i><i id='4' class='fa fa-star' aria-hidden='true'></i><i id='5' class='fa fa-star' aria-hidden='true'></i>"
-    }
+    }*/
 
 
   //var appendText = ele.value + "<br><i id='1' class='fa fa-star' aria-hidden='true'></i><i id='2' class='fa fa-star' aria-hidden='true'></i><i id='3' class='fa fa-star' aria-hidden='true'></i><i id='4' class='fa fa-star' aria-hidden='true'></i><i id='5' class='fa fa-star' aria-hidden='true'></i>";
 
 }
+
+
